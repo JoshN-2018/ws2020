606b1f6dbb6846cad549ebec18ac4d907515a638
Josh's mac <joshsmac@MacBook-Pro-van-Joshs.local>
2020-05-18 17:12:17 +0200

    removed all sticky everything from the code base — remove interference when building new components

--- resources/js/script.js
+++ resources/js/script.js
@@ -28,50 +28,8 @@ function off() {
 
 
 
-// Sticky headers
 
-window.onscroll = function() {stickyFunction()};
-
-// Get the header
-   // small
-var header = document.getElementById("icon-contact");
-var content = document.getElementById("stick-icon-cont");
-   // medium / large
-var headerLarge = document.getElementById("sticky-contact");
-var gap = document.getElementById("container-case-stick");
-
-
-// Get the offset position of the navbar
-   // small
-var sticky = header.offsetTop;
-   // large
-var stickyLarge = headerLarge.offsetTop;
-
-
-function stickyFunction() {
-  if (window.pageYOffset > sticky || stickyLarge) {
-    // small
-    header.classList.add("stickyM");
-    content.classList.add("shift");
-    content.style.paddingTop = "1.1rem";
-
-    // large
-    headerLarge.classList.add("sticky");
-    gap.style.marginBottom = "0px";
-  } else {
-    // small
-    header.classList.remove("stickyM");
-    content.classList.remove("shift");
-    content.style.paddingTop = "0.4rem";
-
-    // large
-    headerLarge.classList.remove("sticky");
-    gap.style.marginBottom = "-196px";
-  }
-}
-
-
-// link highlighting
+// link highlighting (UPDATE THIS FOR NEW LINKS!!!)
 
 // nav link ids
 var caseLink = document.getElementById("cases-link");
@@ -197,7 +155,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
    // call the above functions
 
-   
+
    addAn1();
    addAn2();
    addAn3();

4bdc53e89595805e7e6558cc0135559e34cb4880
Josh's mac <joshsmac@MacBook-Pro-van-Joshs.local>
2020-05-23 09:36:12 +0200

    debounce is working!

--- resources/js/script.js
+++ resources/js/script.js
@@ -211,6 +211,7 @@ var breakOne = 720
 var breakTwo = 1100
 var breakThree = 1440
 
+/*
 function breakHandler() {
 
    // determine screen breakpoints and assign base values
@@ -285,17 +286,31 @@ function breakHandler() {
 // call on resize
  window.addEventListener('resize', breakHandler);
 
+*/
+
+function debounce(func, wait, immediate) {
+	var timeout;
+	return function() {
+		var context = this, args = arguments;
+		var later = function() {
+			timeout = null;
+			if (!immediate) func.apply(context, args);
+		};
+		var callNow = immediate && !timeout;
+		clearTimeout(timeout);
+		timeout = setTimeout(later, wait);
+		if (callNow) func.apply(context, args);
+	};
+};
 
 
 
+var myEfficientFn = debounce(function() {
+	console.log("fired!");
+}, 250);
 
 
-
-
-
-
-
-
+window.addEventListener('resize', myEfficientFn);
 
 
 

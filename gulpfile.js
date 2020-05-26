02a14006b42b83f33d6d54f7fc6a93c556275539
Josh's mac <joshsmac@MacBook-Pro-van-Joshs.local>
2020-05-26 17:35:26 +0200

    gulp is working

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,11 @@
 // set up
+
 const gulp = require("gulp");
 const imagemin = require("gulp-imagemin");
 const pngquant = require("imagemin-pngquant");
 const mozjpeg = require('imagemin-mozjpeg');
 
-minify function
+
 function imgCompressor() {
    return gulp
    .src("resources/images/*")
@@ -24,14 +25,7 @@ gulp.task("default", gulp.series("imgCompressor","watch"));
 
 
 
-// // minify function
-// gulp.task('default', function(done) => {
-//   gulp.src('resources/images/*')
-//     .pipe(imagemin([
-//       pngquant({quality: [0.5, 0.5]}),
-//       mozjpeg({quality: 50})
-//     ]))
-//     .pipe(gulp.dest("public/resources/images"))
-// });
+
+
 
 // .pipe(plugins.rename({ extname: '.webp' }))

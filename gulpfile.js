0afbd82adec2c2594fc51edc6652cb89e2555414
Josh's mac <joshsmac@MacBook-Pro-van-Joshs.local>
2020-05-27 21:07:11 +0200

    attempt 3

--- gulpfile.js
+++ gulpfile.js
@@ -1,27 +1,46 @@
 // set up
 
-const gulp = require("gulp");
-const imagemin = require("gulp-imagemin");
-const pngquant = require("imagemin-pngquant");
-const mozjpeg = require('imagemin-mozjpeg');
+var gulp = require("gulp"),
+    postcss = require("gulp-postcss"),
+    autoprefixer = require("autoprefixer"),
+    browserSync = require("browser-sync").create();
 
 
-function imgCompressor() {
-   return gulp
-   .src("resources/images/*")
-   .pipe(imagemin({optimizationLevel: 1}))
-   .pipe(gulp.dest("public/resources/images"));
-}
+ var paths = {
+     styles: {
+         // By using styles/**/*.sass we're telling gulp to check all folders for any sass file
+         src: "public/resources/css/*.css",
+         // Compiled files will end up in whichever folder it's found in (partials are not compiled)
+         dest: "public/resources/css"
+     }
+ };
+
+ function style() {
+     return gulp
+         // Use postcss with autoprefixer and compress the compiled file using cssnano
+         .pipe(postcss([autoprefixer()]))
+         // Add browsersync stream pipe after compilation
+         .pipe(browserSync.stream());
+ }
 
 
-gulp.task("imgCompressor", imgCompressor);
 
-
-gulp.task("watch", () => {
-   gulp.watch("public/resources/images", imgCompressor);
-});
-
-gulp.task("default", gulp.series("imgCompressor","watch"));
+// function imgCompressor() {
+//    return gulp
+//    .src("resources/images/*")
+//    .pipe(imagemin({optimizationLevel: 1}))
+//    .pipe(gulp.dest("public/resources/images"));
+// }
+//
+//
+// gulp.task("imgCompressor", imgCompressor);
+//
+//
+// gulp.task("watch", () => {
+//    gulp.watch("public/resources/images", imgCompressor);
+// });
+//
+// gulp.task("default", gulp.series("imgCompressor","watch"));
 
 
 

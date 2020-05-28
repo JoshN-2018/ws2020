34e364b9f4e4118ade629344cefb87afb0c5b0af
Josh's mac <joshsmac@MacBook-Pro-van-Joshs.local>
2020-05-28 08:02:09 +0200

    attempt 4

--- gulpfile.js
+++ gulpfile.js
@@ -1,27 +1,77 @@
 // set up
+// const gulp = require('gulp');
+// const imagemin = require('gulp-imagemin');
+//
+// exports.default = () => (
+//     gulp.src('resources/images/*')
+//         .pipe(imagemin())
+//         .pipe(gulp.dest('public/resources/images'))
+// );
 
-var gulp = require("gulp"),
-    postcss = require("gulp-postcss"),
-    autoprefixer = require("autoprefixer"),
-    browserSync = require("browser-sync").create();
+
+// set up
+const gulp = require("gulp");
+const imagemin = require("gulp-imagemin");
+const pngquant = require("imagemin-pngquant");
+const mozjpeg = require('imagemin-mozjpeg');
+
+// minify function
+function imgCompressor() {
+   return gulp
+   .src("resources/images/*")
+   .pipe(imagemin())
+   .pipe(gulp.dest("public/resources/images"));
+}
+
+// function call
+gulp.task("imgCompressor", imgCompressor);
+
+//watch function
+gulp.task("watch", () => {
+   gulp.watch("public/resources/images", imgCompressor);
+});
+
+gulp.task("default", gulp.series("imgCompressor","watch"));
 
 
- var paths = {
-     styles: {
-         // By using styles/**/*.sass we're telling gulp to check all folders for any sass file
-         src: "public/resources/css/*.css",
-         // Compiled files will end up in whichever folder it's found in (partials are not compiled)
-         dest: "public/resources/css"
-     }
- };
+// custom code
+/*
+.pipe(imagemin([
+    imagemin.gifsicle({interlaced: true}),
+    imagemin.mozjpeg({quality: 75, progressive: true}),
+    imagemin.optipng({optimizationLevel: 5}),
+    imagemin.svgo({
+        plugins: [
+            {removeViewBox: true},
+            {cleanupIDs: false}
+        ]
+    })
+]))
+*/
 
- function style() {
-     return gulp
-         // Use postcss with autoprefixer and compress the compiled file using cssnano
-         .pipe(postcss([autoprefixer()]))
-         // Add browsersync stream pipe after compilation
-         .pipe(browserSync.stream());
- }
+
+// var gulp = require("gulp"),
+//     postcss = require("gulp-postcss"),
+//     autoprefixer = require("autoprefixer"),
+//     browserSync = require("browser-sync").create();
+//
+//
+//  var paths = {
+//      styles: {
+//          // By using styles/**/*.sass we're telling gulp to check all folders for any sass file
+//          src: "public/resources/css/*.css",
+//          // Compiled files will end up in whichever folder it's found in (partials are not compiled)
+//          dest: "public/resources/css"
+//      }
+//  };
+//
+//  function style() {
+//      return gulp
+//          // Use postcss with autoprefixer and compress the compiled file using cssnano
+//          .pipe(postcss([autoprefixer()]))
+//          // Add browsersync stream pipe after compilation
+//          .pipe(browserSync.stream());
+//  }
 
 
 

ab761aaf64b14eb19bccf6bb161ef95c7ace46ce
Josh's mac <joshsmac@MacBook-Pro-van-Joshs.local>
2020-06-02 11:08:07 +0200

    gulp is working and watching correctly

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,122 @@
+// RUN
+
+const { series } = require('gulp');
+const { src, dest } = require('gulp');
+const gulpPngquant = require('gulp-pngquant');
+const changed = require('gulp-changed');
+const { watch } = require('gulp');
+
+function compress(cb) {
+   return src('resources/images/*.png')
+     .pipe(gulpPngquant({
+      quality: '65-80'
+     }))
+     .pipe(dest('docs/resources/images'));
+
+  cb();
+}
+
+// exposing the task
+exports.compress = compress
+
+// exposing a task called build which calls whatever tasks are placed in the folder
+
+exports.build = series(compress);
+
+
+
+// WATCH
+
+function compressChanged(cb) {
+   return src('resources/images/*.png')
+      // filtering pipe to 'changed; files
+     .pipe(changed('docs/resources/images'))
+     .pipe(gulpPngquant({
+      quality: '65-80'
+     }))
+     .pipe(dest('docs/resources/images'));
+
+  cb();
+}
+
+function watchImages() {
+  watch('docs/resources/images', compressChanged);
+};
+
+exports.watchImages = watchImages
+exports.watcher = series(watchImages);
+
+
+
+
+// old code
+  // watch('docs/resources/images').on("change", compress);
+
+
+
+
+
+
+
+
+
+//
+// const { src, dest } = require('gulp');
+// const uglify = require('gulp-uglify');
+// const rename = require('gulp-rename');
+//
+// exports.default = function() {
+//   return src('src/*.js')
+//     // The gulp-uglify plugin won't update the filename
+//     .pipe(uglify())
+//     // So use gulp-rename to change the extension
+//     .pipe(rename({ extname: '.min.js' }))
+//     .pipe(dest('output/'));
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
 // setup
 
 var gulp = require('gulp');
@@ -40,7 +159,7 @@ gulp.task('video', () => {
 
 
 
-
+/*
 
 
 
